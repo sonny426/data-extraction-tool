2ee6671d97c82ec61e6d402b6ed5a2723385e4d1
Sonny Smith <sonnyupworkmail1234@gmail.com>
2024-07-01 10:30:13 -0400

    update secret key

--- backend/CoreRoot/settings.py
+++ backend/CoreRoot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lyau!2p1yqktfrjf5dm2m%o9#gdud_v#e3&x_cm-7vxln(n(e#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
