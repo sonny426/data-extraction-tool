bb4c5a8b9a269a745fb0810a926dd939b4b99b6c
Sonny Smith <sonnyupworkmail1234@gmail.com>
2024-07-16 02:42:14 -0400

    disable home page

--- frontend/src/pages/dashboard/home.jsx
+++ frontend/src/pages/dashboard/home.jsx
@@ -30,7 +30,7 @@ import { CheckCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
 export function Home() {
   return (
     <div className="mt-12">
-      <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
+      {/* <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
         {statisticsCardsData.map(({ icon, title, footer, ...rest }) => (
           <StatisticsCard
             key={title}
@@ -250,7 +250,7 @@ export function Home() {
             )}
           </CardBody>
         </Card>
-      </div>
+      </div> */}
     </div>
   );
 }
