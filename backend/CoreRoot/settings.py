f7835c5c4c1b5c9e9de10de5475b52c946e3ad92
Sonny Smith <sonnyupworkmail1234@gmail.com>
2024-07-22 15:55:30 -0400

    add docker file

--- backend/CoreRoot/settings.py
+++ backend/CoreRoot/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+CORS_ALLOW_ALL_ORIGINS = True
+
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'core_user.User'
 
@@ -83,8 +85,8 @@ REST_FRAMEWORK = {
 }
 
 # CELERY settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Use your Redis URL
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'  # Use your Redis URL
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
